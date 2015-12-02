b898458c40cd3badab5a3fc684e2fc6e6ce8c79b
Skander <skander.kc@gmaill.com>
2015-12-01 23:42:08 -0500

    Now can retrieve photos from a specified folder

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ print '================================================'
 print
 print "Veuillez d'abord entrer quelques informations..."
 
+# Récupérer le dossier contenant les photos
+# Dans ce dossier, on devrait retrouver pleins de dossier ayant comme nom l'index des participants
+root_dir_name = raw_input("- Indiquer le nom du dossier contenant les photos (laisse vide si actuel): ")
+
+if os.path.isdir(root_dir_name):
+    photos_dir_content = os.listdir(root_dir_name)
+else:
+    photos_dir_content = os.listdir(os.curdir)
+
+print photos_dir_content
+
 # Récupérer fichier CSV
 csv_file_name = raw_input("- Indiquer le nom du fichier CSV contenant les emails: ")
 
@@ -49,33 +60,42 @@ with open(csv_file_name, 'rb') as csv_file:
 
         print index, name, email
 
-        # Chercher nom dossier ayant le même numero que l'index
-        # Récupérer le nom des images
+        #Chercher nom dossier ayant le même numero que l'index
+        for directory in photos_dir_content:
+            directory_path = root_dir_name + "/" + directory
+            # Youpi ! On a trouvé le participant et son dossier contenant les photos
+            # On peut maintenant envoyer ses photos !
+            if os.path.isdir(directory_path) and directory == index:
+                photos = os.listdir(directory_path)
 
+                # Ne garder que les images
+                for file in photos:
+                    if not (file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png")):
+                        photos.remove(file)
 
-
-
-server_name = raw_input("- Entrer le serveur de messagerie ('g' pour Gmail): ")
-
-if server_name == 'g' or server_name == 'G':
-    server_smtp = 'smtp.gmail.com'
-    port = 587
-
-email = raw_input('- Votre email: ')
-password = getpass('- Your mot de passe: ')
-to_email = raw_input(' - Destination email: ')
-to_name = raw_input(' - Name of destination: ')
-subject = 'Sending mail easily with Python'
-message = 'here is the message body'
-attachments = [sys.argv[0]]
-
-print 'Connecting to server...'
-server = EmailConnection(server_smtp, port, email, password)
-print 'Preparing the email...'
-email = Email(email, to_email, subject, message)
-print 'Sending...'
-server.send(email)
-print 'Disconnecting...'
-server.close()
-print 'Done!'
+                print photos
+#
+# server_name = raw_input("- Entrer le serveur de messagerie ('g' pour Gmail): ")
+#
+# if server_name == 'g' or server_name == 'G':
+#     server_smtp = 'smtp.gmail.com'
+#     port = 587
+#
+# email = raw_input('- Votre email: ')
+# password = getpass('- Your mot de passe: ')
+# to_email = raw_input(' - Destination email: ')
+# to_name = raw_input(' - Name of destination: ')
+# subject = 'Sending mail easily with Python'
+# message = 'here is the message body'
+# attachments = [sys.argv[0]]
+#
+# print 'Connecting to server...'
+# server = EmailConnection(server_smtp, port, email, password)
+# print 'Preparing the email...'
+# email = Email(email, to_email, subject, message)
+# print 'Sending...'
+# server.send(email)
+# print 'Disconnecting...'
+# server.close()
+# print 'Done!'
 
